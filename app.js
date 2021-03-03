39db574db59301b5d5ff46491f986e5115ab31d4
jobu7820 <jobu7820@colorado.edu>
2021-03-02 21:43:04 -0700

    Database Connection
    Added wockyStonks DB connection for app.js. Unsure if it's connected properly, working on checking logs to see if successful connection is made.
    
    Created empty database model to plan out schema.

--- app.js
+++ app.js
@@ -4,8 +4,27 @@
 
 require('dotenv').config();
 const K = require(__dirname + "/K.js");
-const express = require('express');
+const express = require('express'); // Express framework
 const bodyParser = require('body-parser');
+// app.use(bodyParser.json()); // Support json encoded bodies
+// app.use(bodyParser.urlencoded({extended: true})); // Support encoded bodies
+
+// Create Database Connection
+var mysql = require('mysql');
+
+/*
+Database Connection Info
+host: Defines ip address of server hosting database
+user: RDS username
+password: RDS username
+port: Port to communicate with database
+*/
+var connection = mysql.createConnection({
+    host: process.env.RDS_HOSTNAME,
+    user: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    port: process.env.RDS_PORT
+});
 
 // Router Modules
 const login = require(__dirname + '/routes/loginRouter.js')
@@ -15,16 +34,26 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-
 app.use('/', login);
 
+// LOG SUCCESS/ERROR FOR DB CONNECTION
+connection.connect(function(err){
+    if(err){
+        console.error('Database connection failed: ' + err.stack);
+        return
+    }
+    console.log('Connected to database.');
+})
 
+// GET REQUESTS
 app.get('/register', (req, res) => {
     res.render('register.ejs');
 });
 app.get('/dashboard', (req, res) => {
     res.render('dashboard.ejs');
 });
+
+// POST REQUESTS
 app.post('/register', (req, res) => {
     console.log('POST: /register');
 });
@@ -36,3 +65,5 @@ app.post('/dashboard', (req, res) => {
 app.listen(K.port, () => {
     console.log(`Server started on port: ${K.port}`);
 });
+
+connection.end();
