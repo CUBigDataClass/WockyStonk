2b5a0bbf93bf2d6350b921a2e24354b72f61e2c1
JonathanPhouminh <jonathanphouminh@Jonathans-MacBook-Pro.local>
2021-03-10 11:28:03 -0700

    added react-router-dom and did basic routing with Login and Dashboard

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import Login from './Login/Login'; 
 import DashBoard from './Dashboard/Dashboard';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div> 
-      <Login /> 
-    </div> 
+    <Router> 
+      <div> 
+      <Switch>
+        <Route path={"/"} exact component={Login} /> 
+        <Route path={"/dashboard"} component={DashBoard} /> 
+      </Switch>
+      </div> 
+    </Router> 
   );
 }
 
