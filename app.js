570838ce9f1af173998ceeee8fffa43a75accaa2
JonathanPhouminh <jonathanphouminh@Jonathans-MacBook-Pro.local>
2021-03-12 22:47:02 -0700

    cleaned up app.js

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-
 // AUTHOR: Jonathan Phouminh
 // DATE: February 27th, 2021
-
 
 require('dotenv').config();
 const K = require(__dirname + "/K.js");
@@ -24,32 +22,13 @@ app.use(express.json());
 app.use('/', login);
 app.use('/register',register)
 
-app.get('/dashboard', (req, res) => {
-    res.send('Something came SLDKFJDS:LKsadadFS');
-});
-
 app.get('/logout', (req,res) => {
     sql.killConnection(); 
 })
 
-
-/* Use this get route to test the db for now, just type it in the url */
-/* sql is the reference to the db handler */
-app.get('/testDB', (req, res) => {
-    console.log("testDB GET"); 
-    res.send("/testDB");
-});
-
-app.post('/dashboard', (req, res) => {
-    console.log('POST: /dashboard');
-});
-
-
 app.listen(K.port, () => {
     console.log(`Server started on port: ${K.port}`);
 });
-
-
 
 /* Export after connection is established so other files can have reference to it */
 module.exports.sql = sql; 
