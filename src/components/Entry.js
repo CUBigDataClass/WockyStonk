009795765513d0e9cade03e0f2090c42a8bfc6c2
JonathanPhouminh <jonathanphouminh@Jonathans-MacBook-Pro.local>
2021-03-17 19:01:58 -0600

    added redux dependency

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -3,18 +3,21 @@ import Register from './Login/Register';
 import DashBoard from './Dashboard/Dashboard';
 import './styles/Login.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Provider } from 'react-redux'
 
 function Entry() {
   return (
-    <Router> 
-      <div> 
-      <Switch>
-        <Route path={"/"} exact component={Login} /> 
-        <Route path={"/dashboard"} component={DashBoard} /> 
-        <Route path={"/register"} component={Register} /> 
-      </Switch>
-      </div> 
-    </Router> 
+    <Provider>
+      <Router> 
+        <div> 
+        <Switch>
+          <Route path={"/"} exact component={Login} /> 
+          <Route path={"/dashboard"} component={DashBoard} /> 
+          <Route path={"/register"} component={Register} /> 
+        </Switch>
+        </div> 
+      </Router> 
+    </Provider>
   );
 }
 
