3611a5a08519598f4431aaa40f4cd4e7d39e6bfe
phoubear <phoubear@pop-os.localdomain>
2021-03-18 10:59:20 -0600

    fixed icon cell bug where there were sliders on the icon text

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,37 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Footer.css";
 import data from "./mock";
 import Ticker from "./Ticker";
 
-class Footer extends React.Component {
-  constructor(props) {
-    super(props);
+function Footer(props) {
+  
+  const [tickers, updateTickers] = useState(data); 
 
-    this.state = {
-      tickers: data
-    };
-  }
-  render() {
-    return (
-      <div className="footer">
-        <Ticker
-          label={this.state.tickers[0].name}
-          index={this.state.tickers[0].index}
-          change={this.state.tickers[0].change}
-        />
-        <Ticker
-          label={this.state.tickers[1].name}
-          index={this.state.tickers[1].index}
-          change={this.state.tickers[1].change}
-        />
-        <Ticker
-          label={this.state.tickers[2].name}
-          index={this.state.tickers[2].index}
-          change={this.state.tickers[2].change}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="footer">
+      <Ticker
+        label={tickers[0].name}
+        index={tickers[0].index}
+        change={tickers[0].change}
+      />
+      <Ticker
+        label={tickers[1].name}
+        index={tickers[1].index}
+        change={tickers[1].change}
+      />
+      <Ticker
+        label={tickers[2].name}
+        index={tickers[2].index}
+        change={tickers[2].change}
+      />
+    </div>
+  );
 }
 export default Footer;
 
