c9fb16c1ce24195b991be1afe2d41adf3bc0d6c4
phoubear <phoubear@pop-os.localdomain>
2021-04-11 12:42:06 -0600

    making footer dynamic, branching off

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,20 +2,22 @@ import React, { useState } from "react";
 import "../styles/Footer.css";
 
 //- Component Imports
-import data from "./mock";
 import Ticker from "./Ticker";
 
 //- Redux Imports
 import { useSelector, useDispatch } from "react-redux";
 
-//- Footer will just read off the values from the state, have something dispatch actions to update
-//- the values of the each index 
 function Footer(props) {
  
-  const dispatch = useDispatch();
+  //- gets access to redux state vars 
   const dow = useSelector((state) => state.dowIndex);
   const nasdaq = useSelector((state) => state.nasdaqIndex);
   const sp = useSelector((state) => state.spIndex);
+  
+  //- dispatch actions to update vars 
+  const dispatch = useDispatch();
+
+  
 
   return (
     <div className="footer">
--- src/components/Footer/mock.js
+++ src/components/Footer/mock.js
@@ -27,23 +27,4 @@ const fmp = require('financialmodelingprep')('c494c521ea2b5688b838372418d58fd2')
 // ];
 
 // console.log(indexes[0]);
-
-const mock = [
-    {
-      name: "DOW",
-      index: 32297.02,
-      change: 3.00000000000
-    },
-    {
-      name: "NASDAQ",
-      index: 13068.83,
-      change: -0.04
-    },
-    {
-      name: "S&P500",
-      index: 3898.81,
-      change: 0.6
-    }
-  ];
-  export default mock;
   
