15187518a54ac5e0d4be88105220630c99266bc8
BaoNguyen <bang4255@colorado.edu>
2021-04-25 22:06:25 -0600

    issues with api

--- src/Twitter.js
+++ src/Twitter.js
@@ -4,26 +4,36 @@ function searchTweets(keyword){
     var client = new Twitter({
         consumer_key: 'uhA1eCdkq3sIKqmGcLP2iHLem',
         consumer_secret: 'LFATI85ROPCSkxqXrJtbBg5gOcJmGrg52hTHdVeFkarXvAKI0w',
-        access_token_key: '1182330351521124352-jq0ncBvXIKi9bFbK1cQlNU6LawjNmM',
-        access_token_secret: 'VFpeSpyZollw4XNmEETCeZ43FL5lD3yYAvtyGZlsaWBag',
-    
+        access_token_key: '1182330351521124352-pROgw76zuSJqJtqyXduoWxWEn3fldr',
+        access_token_secret: 'VZ5yi61g8AiUjq0DdiCHiCUkODVzFxLzx6YfP0pf9zlPd',
     });
+
+    let id = "";
+    let userName = "";
     let embed = " ";
-    let htmlCode = '';
+    let htmlCode = "";
     //taken from https://medium.com/@federicojordn/simplertapp-twitter-search-api-with-node-js-29e4664db299
-    client.get('search/tweets', {q: keyword, count: 2, result_type: 'popular'}, function(error, tweets, response) {
+    client.get('search/tweets', {q: keyword, count: 1, result_type: 'popular'}, getUserInfo);
+    function getUserInfo(err,tweets,response){
+        if(err){
+            console.log(err)
+            throw(err)
+        }
         tweets.statuses.forEach(function(tweet){
-            let id = (tweet.id_str) // extracts id of tweet
-            let userName = (tweet.user.screen_name) // extracts username
-
-            embed = `https://twitter.com/${userName}/status/${id}` //link needed for embeded html code
-
-            client.get('statuses/oembed', {url: embed}, function(error, data, response){
-                htmlCode = data.html;
-                
-            });
-            console.log(htmlCode);
-        });
-     });
+            id = tweet.id_str 
+            userName = tweet.user.screen_name
+        })
+        embed = id + userName;
+        // embed = `https://twitter.com/${userName}/status/${id}` //link needed for embeded html code
+        //      client.get('statuses/oembed', {url: embed}, function(error, data, response){
+         
+        //  htmlCode = data.html
+        //  console.log(htmlCode)
+        console.log(embed)
+        return embed
+    //  }); 
+     
+    }
 }
-searchTweets('#stockmarket')
+searchTweets("#stockmarket")
+module.exports.searchTweets = searchTweets;
---
+++ src/components/twitter/table.jsx
@@ -0,0 +1,3 @@
+import React, { useState } from "react";
+import "../styles/table.css";
+import Twitter from '../../src/Twitter'
